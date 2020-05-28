67ee74366adc972729879ae28180e0cd27e676c4
Glost <gjf734128912@gmail.com>
2020-05-28 10:46:34 +0800

    test redis

---
+++ web/index_reged.php
@@ -0,0 +1,30 @@
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>首页</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="index.css">
+    <script src="main.js"></script>
+</head>
+<body>
+  <div class="header clearfix">
+    <div class="main_header">
+      <h1>简历仓库</h1>
+    </div>
+    <div class="right_header">
+      <ul class="header_list">
+        <li ><a href="user_reg.php">我的简历</a></li>
+        <li><a href="user_signin.php">登出</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="main_body clearfix">
+    <ul class="cv_feeds">
+    </ul>
+  </div>
+  
+</body>
+</html>
---
+++ web/redis_test.php
@@ -0,0 +1,9 @@
+<?php
+    $redis = new Redis();
+    $redis->connect('cv_redis');
+    echo "Connection to server successfully</br>";
+   //设置 redis 字符串数据
+   $redis->set("tutorial-name", "Redis tutorial");
+   // 获取存储的数据并输出
+   echo "Stored string in redis:: " . $redis->get("tutorial-name");
+?>
