76f181f951af963d37f0230f809daf6b46aae0d2
Glost <gjf734128912@gmail.com>
2020-07-16 16:39:48 +0800

    unknown

--- web/redis_test.php
+++ web/redis_test.php
@@ -1,9 +1,11 @@
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
 <?php
     $redis = new Redis();
     $redis->connect('cv_redis');
     echo "Connection to server successfully</br>";
    //设置 redis 字符串数据
-   $redis->set("tutorial-name", "Redis tutorial");
+   $redis->set("testkey", "test value");
    // 获取存储的数据并输出
-   echo "Stored string in redis:: " . $redis->get("tutorial-name");
+   echo "Stored string in redis:: " . $redis->get("testkey");
 ?>
